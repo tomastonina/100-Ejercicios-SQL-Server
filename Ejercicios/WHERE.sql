8d2d1383be22595ae2f56f91eb4b26f573bb073e
Tomas Tonina <tomastonina@gmail.com>
2021-08-12 18:01:42 -0300

    v12.8.21-2
    Ejercicios WHERE realizados

--- Ejercicios/WHERE.sql
+++ Ejercicios/WHERE.sql
@@ -4,4 +4,10 @@ use sakila;
 
       Filtra la información donde title sea IMPACT ALADDIN*/
 
-select release_year as Fecha_de_lanzamiento, description as descripcion from film;
+select release_year as Fecha_de_lanzamiento, description as descripcion from film where title = 'IMPACT ALADDIN';
+
+/*    Consulta la tabla payment de la base de datos sakila.
+
+      Filtra la información donde amount sea mayor a 0.99.*/
+
+select * from payment where amount > 0.99 order by amount;
